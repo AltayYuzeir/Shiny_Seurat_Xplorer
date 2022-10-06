0430e30ddaeef9cb0e49c195b196ad46a4cd2a8f
Altay Yuzeir <34285813+AltayYuzeir@users.noreply.github.com>
2022-10-06 08:59:26 +0200

    Add files via upload

---
+++ Docker/install.R
@@ -0,0 +1,15 @@
+# Use this file to install your requirements
+options(warn = 2) # stop installation  at warning // without this failing package installations will most likely be overseen
+##############################
+### Examples on how to use: ##
+#
+# install.packages("PACKAGE_NAME")
+# devtools::install_github("GITREPO")
+#
+##############################
+
+
+install.packages("shiny")
+install.packages("Seurat")
+install.packages("SeuratObject")
+install.packages("sp")
